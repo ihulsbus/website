73d7ad559722fe59e7030bf6dc7ae954b69eadb0
Ian Hulsbus <ian@hulsbus.be>
2021-07-29 18:13:35 +0200

    Initial website

---
+++ src/components/MainPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Hi, Welcome</h1>
+    <span>
+        <p>Unfortunately there's nothing here. There should be, but I've not had the time to make awesome content for this site.</p>
+        <p>In the mean time, take a look at my Github profile if you want.</p>
+        <Button label="Github" icon="pi pi-github" @click="btnClick" />
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'MainPage',
+    methods : {
+        btnClick() {
+            window.open("https://github.com/ihulsbus");
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
 
-createApp(App).mount('#app')
+import 'primevue/resources/themes/vela-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+const app = createApp(App);
+
+app.use(PrimeVue, { ripple: true });
+
+app.component('Button', Button);
+
+app.mount('#app');
