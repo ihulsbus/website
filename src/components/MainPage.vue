3c79aefc15fe0c86d400160d35481ca002ccffc1
Ian Hulsbus <ian@hulsbus.be>
2023-03-06 18:55:48 +0000

    Add Linkedin

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,8 +2,9 @@
     <h1>Hi, Welcome</h1>
     <span>
         <p>Unfortunately there's nothing here. There should be, but I've not had the time to make awesome content for this site.</p>
-        <p>In the mean time, take a look at my Github profile if you want.</p>
-        <Button label="Github" icon="pi pi-github" @click="btnClick" />
+        <p>In the mean time, take a look at my Github or LinkedIn profile if you want.</p>
+        <Button style="margin-right: 0.5rem;" class="github" label="Github" icon="pi pi-github" @click="github" aria-label="Github"/>
+        <Button class="linkedin" label="LinkedIn" icon="pi pi-linkedin" @click="linkedin" aria-label="LinkedIn"/>
     </span>
 </template>
 
@@ -11,13 +12,18 @@
 export default {
     name: 'MainPage',
     methods : {
-        btnClick() {
+        github() {
             window.open("https://github.com/ihulsbus");
+        },
+        linkedin() {
+            window.open("https://linkedin.com/in/ianhulsbus");
         }
     }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style>
+.p-button {
+        margin-right: 0.5rem;
+    }
 </style>
